6b6aec679f9912fa717f13b4d9497fe0e108bb97
lwcreel <landon.creel@icloud.com>
2024-09-26 23:17:53 -0600

    Adding pagination, new post

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,27 +1,41 @@
-import Layout from "../components/layout";
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from "../lib/posts";
+import Layout from "../../components/layout";
+import utilStyles from '../../styles/utils.module.css';
+import { getSortedPostsData } from "../../lib/posts";
 import Link from "next/link";
-import Date from '../components/date';
+import Date from '../../components/date';
 import Head from "next/head";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 
 
 export async function getStaticProps() {
     const allPostsData = await getSortedPostsData();
     return {
         props: {
-            allPostsData,
-        },
+            allPostsData
+        }
     };
 }
+
 
 // TODO: Paginate blog posts once you've got enough
 // Also, maybe find a more maintanable method of making them? idk
 
 
 export default function Blog({ allPostsData }) {
+   
+    let [page, setPage] = useState(1);
 
+    let posts = allPostsData[0].slice((page - 1) * allPostsData[2], page * allPostsData[2]);
 
+    const handleNext = () => {
+        page + 1  > allPostsData[1] ? setPage(page) : setPage(page++);
+        console.log("page "  + page);
+    }
+    const handlePrev = () => {
+        page - 1 > 0 ? setPage(page--) : setPage(page);
+        console.log("page " + page);
+    }
 
     return (
         <Layout>
@@ -32,7 +46,7 @@ export default function Blog({ allPostsData }) {
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
                 <ul className={utilStyles.list}>
-                    {allPostsData.map(({ id, date, title }) => (
+                    {posts.map(({ id, date, title }) => (
                         <li className={utilStyles.listItem} key={id}>
                             <Link href={`/posts/${id}`}>{title}</Link>
                             <br />
@@ -43,6 +57,9 @@ export default function Blog({ allPostsData }) {
                     ))}
                 </ul>
             </section>
+            <Button variant="dark" onClick={handlePrev} hidden={page == 1}>prev</Button>
+            <Button variant="dark" onClick={handleNext} hidden={page == allPostsData[1]}>next</Button>
         </Layout>
     )
 }
+
