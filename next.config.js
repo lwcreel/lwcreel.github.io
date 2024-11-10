1d0e4fbe155958fe3cc940d4c1d1538c2218ddc6
lwcreel <landon.creel@icloud.com>
2024-11-09 18:22:33 -0700

    Revert "attempting to fix failed deploy 2"
    This reverts commit 99341164fffd423f53f829d53617bb853b8def65.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-  //output: "export",
-  images: {
-    unoptimized: true,
-  },
-};
-
-module.exports = nextConfig;
+//const nextConfig = {
+//  output: "export",
+//};
+//
+//module.exports = nextConfig;
